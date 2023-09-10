2d3c100636a387a7194ad9fc83ac2a5f910400f7
bArtOzzzz <breakyourbrain55@gmail.com>
2023-09-10 13:15:20 +0300

    Added login validation and endpoint implementation

--- src/Components/LoginPage.tsx
+++ src/Components/LoginPage.tsx
@@ -1,18 +1,69 @@
-const LoginPage = () => {
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+
+const LoginPage: React.FC = () => {
+    const navigate = useNavigate();
+    const [loginError, setLoginError] = useState('');
+
+    interface LoginValues {
+        email: string;
+        password: string;
+      }
+    
+    const initialValues: LoginValues = {
+        email: '',
+        password: '',
+    };
+
+    const validationSchema = Yup.object({
+        email: Yup.string().email('Invalid email address').required('Required'),
+        password: Yup.string().required('Required'),
+      });
+    
+      const onSubmit = async (values: LoginValues) => {
+        try {
+          const response = await axios.post('https://localhost:7196/api/Authentication/Login', values);
+          localStorage.setItem('token', response.data.accessToken);
+          navigate('/home');
+        } catch (error: any) {
+          setLoginError(error.response.data.message);
+        }
+      };
+
     return (
         <>
-        <form action="">
-            <p className="title-login">Login</p>
-            <div className="container">
-                <div className="email-container">
-                    <input type="text" placeholder="Email" name="email" />
-                </div>
-                <div className="password-container">
-                    <input type="password" placeholder="Password" name="password"/>
-                </div>
-                <button className="submit-btn" type="submit">Submit</button>
-            </div>
-        </form>
+        <Formik initialValues={initialValues} 
+                validationSchema={validationSchema} 
+                onSubmit={onSubmit}>
+            {({isSubmitting}) => (
+                <Form>
+                    <p className="title-login">Login</p>
+                    <div className="container">
+                        <div className="email-container">
+                            <Field type="email" name="email" placeholder="Email" />
+                            <div className='error-message'>
+                                <ErrorMessage name="email" />
+                            </div>
+                        </div>
+                        <div className="password-container">
+                            <Field type="password" name="password" placeholder="Password" />
+                            <div className='error-message'>
+                                <ErrorMessage name="password" />
+                            </div>
+                        </div>
+                        {
+                            loginError && (
+                                <div style={{ color: 'red' }}>{loginError}</div>
+                            )
+                        }
+                        <button type="submit" className="submit-btn" disabled={isSubmitting}>Submit</button>
+                    </div>
+                </Form>
+            )}
+        </Formik>
         </>
     )
 }
